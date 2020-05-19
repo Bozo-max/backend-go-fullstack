00793c94ac354d90a960f1b96a21e66676604c41
Bozo-max <benjamin.fargeton@hotmail.fr>
2020-05-19 21:24:42 +0200

    PUT route

--- app.js
+++ app.js
@@ -19,6 +19,11 @@ app.use((req,res, next)=>{
     next();
 })
 .use(bodyParser.json())
+.put('/api/stuff/:id', (req,res,next)=>{
+    Thing.updateOne({_id: req.params.id}, {...req.body, _id:req.params.id})
+    .then(()=>res.status(201).json({message:'Thing updated'}))
+    .catch(error=>res.status(400).json(error));
+})
 .post('/api/stuff/', (req,res,next)=>{
     delete req.body._id;
     const thing = new Thing({...req.body});
@@ -26,7 +31,12 @@ app.use((req,res, next)=>{
     .then(()=>res.status(201).json({message: 'Thing created'}))
     .catch(error=>res.status(400).json({error}));
 })
-.use('/api/stuff/',(req,res,next)=>{
+.get('/api/stuff/:id', (req, res ,next)=>{
+    Thing.findOne({_id:req.params.id})
+    .then(thing=>res.status(200).json(thing))
+    .catch(error=>res.status(404).json({error}));
+})
+.get('/api/stuff/',(req,res,next)=>{
     Thing.find()
     .then(things=>res.status(200).json(things))
     .catch(error=>res.status(400).json({error}));
