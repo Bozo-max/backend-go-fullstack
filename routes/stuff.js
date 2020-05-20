ea2c5ef234f361306002208982425d834e1c1233
Bozo-max <benjamin.fargeton@hotmail.fr>
2020-05-20 11:14:18 +0200

    authentication middleware to secure stuff access

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const stuffController = require('../controllers/stuff');
+const auth = require('../middleware/auth');
 
-router.put('/:id', stuffController.modifyThing)
-    .delete('/:id', stuffController.deleteThing)
-    .post('/', stuffController.createThing)
-    .get('/:id', stuffController.getOneThing)
-    .get('/', stuffController.getAllThings)
+router.put('/:id', auth, stuffController.modifyThing)
+    .delete('/:id', auth, stuffController.deleteThing)
+    .post('/', auth, stuffController.createThing)
+    .get('/:id', auth, stuffController.getOneThing)
+    .get('/', auth, stuffController.getAllThings)
 
 module.exports = router;
